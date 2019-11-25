e452a89b4cdc6b23375c21e6c2065bbd84466990
Sergiu Pocol <spocol@gra-login3.graham.sharcnet>
2019-11-24 23:40:14 -0500

    More dataset support

--- code_nma/functions/prep.data.atrial.R
+++ code_nma/functions/prep.data.atrial.R
@@ -1,45 +1,45 @@
-prep.data.smoking <- function(){
+prep.data.atrial <- function(){
   
-  smoking.data <- read.csv("smokingData.csv")
+  atrial.data <- read.csv("atrialData.csv")
   
-  trt.names <- as.character(unique(smoking.data$treatment)) #rownames(gemtc::smoking$treatments)
+  trt.names <- as.character(unique(atrial.data$treatment)) #rownames(gemtc::atrial$treatments)
   trt.numbers <- 1:length(trt.names)
   
-  study.names <- unique(smoking.data$study)
+  study.names <- unique(atrial.data$study)
   study.numbers <- 1:length(study.names)
   
   trt.names.data.frame <- data.frame(cbind(trt.numbers, trt.names))
 
 
-  n.arms <- table(smoking.data$study)
+  n.arms <- table(atrial.data$study)
   
   which.2.arms <- as.numeric(names(which(n.arms == 2)))
   
-  smoking.2.arms.only <- smoking.data[which(smoking.data$study %in% which.2.arms),]
+  atrial.2.arms.only <- atrial.data[which(atrial.data$study %in% which.2.arms),]
 
 
 
-  cat("Complete smoking data\n")
-  print(smoking.data)
-  cat("Smoking data with only 2 arms >>>\n")
-  print(smoking.2.arms.only)
+  cat("Complete atrial data\n")
+  print(atrial.data)
+  cat("atrial data with only 2 arms >>>\n")
+  print(atrial.2.arms.only)
 
 
-  smoking.data <- smoking.2.arms.only
+  atrial.data <- atrial.2.arms.only
   
-  smoking.data$t.id <- as.numeric(as.character(mapvalues(smoking.data$treatment,
+  atrial.data$t.id <- as.numeric(as.character(mapvalues(atrial.data$treatment,
                                                             trt.names, 
                                                             trt.numbers)))
   
-  smoking.data$s.id <- as.numeric(as.character(mapvalues(smoking.data$study,
+  atrial.data$s.id <- as.numeric(as.character(mapvalues(atrial.data$study,
                                                             study.names, 
                                                             study.numbers)))
   
   
-  smoking.data <- rename(smoking.data, c("responders"="r", "sampleSize"="n"))
+  atrial.data <- rename(atrial.data, c("responders"="r", "sampleSize"="n"))
   
   plot.network(paste0(output.folder,"/plots/raw_data_network.pdf"), 
-               smoking.data, 
+               atrial.data, 
                "", 
                trt.names.data.frame)
   
@@ -47,6 +47,6 @@ prep.data.smoking <- function(){
                               trt.names.data.frame,
                               ref.trt)
   
-  return(list("data"=smoking.data, "treatments"=trt.names.data.frame))
+  return(list("data"=atrial.data, "treatments"=trt.names.data.frame))
   
 }
---
+++ code_nma/functions/prep.data.thrombolytic.R
@@ -0,0 +1,45 @@
+prep.data.thrombolytic <- function(){
+  
+  thrombolytic.data <- read.csv("thrombolyticData.csv")
+  
+  trt.names <- as.character(unique(thrombolytic.data$treatment)) #rownames(gemtc::smoking$treatments)
+  trt.numbers <- 1:length(trt.names)
+  
+  study.names <- unique(thrombolytic.data$study)
+  study.numbers <- 1:length(study.names)
+  
+  trt.names.data.frame <- data.frame(cbind(trt.numbers, trt.names))
+
+
+  n.arms <- table(thrombolytic.data$study)
+  
+  which.2.arms <- as.numeric(names(which(n.arms == 2)))
+  
+  thrombolytic.2.arms.only <- thrombolytic.data[which(thrombolytic.data$study %in% which.2.arms),]
+
+  thrombolytic.data <- thrombolytic.2.arms.only
+
+  
+  thrombolytic.data$t.id <- as.numeric(as.character(mapvalues(thrombolytic.data$treatment,
+                                                            trt.names, 
+                                                            trt.numbers)))
+  
+  thrombolytic.data$s.id <- as.numeric(as.character(mapvalues(thrombolytic.data$study,
+                                                            study.names, 
+                                                            study.numbers)))
+  
+  
+  thrombolytic.data <- rename(thrombolytic.data, c("responders"="r", "sampleSize"="n"))
+  
+  plot.network(paste0(output.folder,"/plots/raw_data_network.pdf"), 
+               thrombolytic.data, 
+               "", 
+               trt.names.data.frame)
+  
+  write.treatment.description(paste0(output.folder,"/treatment-description.txt"),
+                              trt.names.data.frame,
+                              ref.trt)
+  
+  return(list("data"=thrombolytic.data, "treatments"=trt.names.data.frame))
+  
+}
